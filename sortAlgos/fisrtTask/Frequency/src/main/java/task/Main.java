3692c5db2c5e659c1aa840068678d6a8f01b0103
splitmindq <mzitraksqq@gmail.com>
2024-12-03 17:13:38 +0300

    cs

--- sortAlgos/fisrtTask/Frequency/src/main/java/task/Main.java
+++ sortAlgos/fisrtTask/Frequency/src/main/java/task/Main.java
@@ -4,7 +4,9 @@ import java.util.*;
 import java.util.logging.Logger;
 
 public class Main {
+
     private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -14,20 +16,19 @@ public class Main {
             words.add(line);
         }
 
-        Map<String ,Integer> wordFrequency = new HashMap<>();
+        Map<String, Integer> wordFrequency = new HashMap<>();
 
         Set<String> wordSet = new HashSet<>();
 
-        for(String word : words){
-            if(wordSet.add(word)){
-                wordFrequency.put(word,1);
-            }
-            else{
-                wordFrequency.put(word,wordFrequency.get(word)+1);
+        for (String word : words) {
+            if (wordSet.add(word)) {
+                wordFrequency.put(word, 1);
+            } else {
+                wordFrequency.put(word, wordFrequency.get(word) + 1);
             }
         }
 
-        List<Map.Entry<String,Integer>> entries = ReverseOrderSort.sort(wordFrequency);
+        List<Map.Entry<String, Integer>> entries = ReverseOrderSort.sort(wordFrequency);
         logger.info(entries.toString());
 
     }
--- sortAlgos/fisrtTask/Frequency/src/main/java/task/ReverseOrderSort.java
+++ sortAlgos/fisrtTask/Frequency/src/main/java/task/ReverseOrderSort.java
@@ -3,19 +3,16 @@ package task;
 import java.util.List;
 import java.util.Map;
 import java.util.*;
+
 public class ReverseOrderSort {
 
-    public static List<Map.Entry<String,Integer>> sort(Map<String,Integer> entries){
+    public static List<Map.Entry<String, Integer>> sort(Map<String, Integer> entries) {
 
-
-        List<Map.Entry<String,Integer>> entriesList = new ArrayList<>(entries.entrySet());
+        List<Map.Entry<String, Integer>> entriesList = new ArrayList<>(entries.entrySet());
         entriesList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
 
         return entriesList;
 
     }
-
-
-
 
 }
